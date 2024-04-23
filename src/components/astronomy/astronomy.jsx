aefd34b0a4a1e005659bed8e4b6312ee28cff9bc
Harshitaa Kashyap <suzimayisbae@gmail.com>
2024-04-23 16:10:45 +0530

    fixes

--- src/components/astronomy/astronomy.jsx
+++ src/components/astronomy/astronomy.jsx
@@ -1,19 +1,46 @@
-import React from "react";
-import { SideBar } from "../elements/sidebar/sidebar"; 
-import { Card1 } from "../elements/card/card";
+import React, { useState } from "react";
+import { SideBar } from "../elements/sidebar/sidebar";
+import { Card1 } from "../elements/card/card1";
+import { Card2 } from "../elements/card/card2";
+import { Card3 } from "../elements/card/card3";
+import { Card4 } from "../elements/card/card4";
+import { Card5 } from "../elements/card/card5";
 import "./astronomy.css";
 
 const AstronomyPage = () => {
-    return (
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div>
-                <SideBar />
-            </div>
-            <div style={{ overflowY: 'auto', maxHeight: '100vh', marginLeft: '30px'}}>
-                <Card1/>
-            </div>
-        </div>
-    );
+  const [selectedCard, setSelectedCard] = useState("");
+
+  const handleItemClick = (item) => {
+    setSelectedCard(item.name);
+  };
+
+  const renderSelectedCard = () => {
+    switch (selectedCard) {
+      case "Ideas":
+        return <Card2 />;
+      case "My Ideas":
+        return <Card3 />;
+      case "Experiences":
+        return <Card4 />;
+      case "My Experiences":
+        return <Card5 />;
+      default:
+        return <Card1 />;
+    }
+  };
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div>
+        <SideBar onItemClick={handleItemClick} />
+      </div>
+      <div
+        style={{ overflowY: "auto", maxHeight: "100vh", marginLeft: "30px" }}
+      >
+        {renderSelectedCard()}
+      </div>
+    </div>
+  );
 };
 
 export default AstronomyPage;
--- src/components/elements/sidebar/sidebar.jsx
+++ src/components/elements/sidebar/sidebar.jsx
@@ -1,95 +1,87 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import "./sidebar.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const menuItems = [
-    {
-        name: "Ideas",
-    },
-    {
-        name: "My Ideas",
-    },
-    {
-        name: "Experiences",
-    },
-    {
-        name: "My Experiences",
-    },
-    {
-        name: "Share",
-        link: "/share"
-    }
+  {
+    name: "Ideas",
+  },
+  {
+    name: "My Ideas",
+  },
+  {
+    name: "Experiences",
+  },
+  {
+    name: "My Experiences",
+  },
+  {
+    name: "Share",
+    link: "/share",
+  },
 ];
 
 const NavHeader = ({ toggleSidebar }) => (
-    <header className="sidebar-header">
-        <button type="button" onClick={toggleSidebar}>
-            <FontAwesomeIcon icon={faBars} />
-            <span></span>
-        </button>
-    </header>
-);
-
-const NavButton = ({
-    onClick,
-    name,
-    isActive,
-    icon,
-    link
-}) => (
-    <button type="button" onClick={() => onClick({ item: name })}>
-        <Link to={link} className={`nav-link ${isActive ? "active" : ""}`} onClick={() => onClick({ item: name })}>
-            <FontAwesomeIcon icon={faMoon} />
-            <span style={{ marginLeft: '8px' }}>{name}</span>
-        </Link>
+  <header className="sidebar-header">
+    <button type="button" onClick={toggleSidebar}>
+      <FontAwesomeIcon icon={faBars} />
+      <span></span>
     </button>
+  </header>
 );
 
-export const SideBar = () => {
-    const [activeItem, setActiveItem] = useState("");
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+const NavButton = ({ onClick, name, isActive, icon, link }) => (
+  <button type="button" onClick={() => onClick({ name })}>
+    <Link to={link} className={`nav-link ${isActive ? "active" : ""}`}>
+      <FontAwesomeIcon icon={faMoon} />
+      <span style={{ marginLeft: "8px" }}>{name}</span>
+    </Link>
+  </button>
+);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
+export const SideBar = ({ onItemClick }) => {
+  const [activeItem, setActiveItem] = useState("");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-        window.addEventListener("resize", handleResize);
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    const handleClick = ({ item }) => setActiveItem(
-        item !== activeItem ? item : ""
-    );
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
     };
 
-    if (windowWidth >= 320 && windowWidth <= 425 && !isSidebarOpen) {
-        return (
-            <NavHeader toggleSidebar={toggleSidebar} />
-        ); 
-    }
+    window.addEventListener("resize", handleResize);
 
-    return (
-        <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-            <NavHeader toggleSidebar={toggleSidebar} />
-            {menuItems.map((item, index) => (
-                <div key={index}>
-                    <NavButton
-                        onClick={handleClick}
-                        name={item.name}
-                        icon={item.icon}
-                        isActive={activeItem === item.name}
-                        link={item.link}
-                    />
-                </div>
-            ))}
-        </aside>
-    );
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleClick = ({ name }) => {
+    setActiveItem(name);
+    onItemClick({ name }); // Pass the clicked item to the onItemClick function
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  if (windowWidth >= 320 && windowWidth <= 425 && !isSidebarOpen) {
+    return <NavHeader toggleSidebar={toggleSidebar} />;
+  }
+
+  return (
+    <aside className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+      <NavHeader toggleSidebar={toggleSidebar} />
+      {menuItems.map((item, index) => (
+        <div key={index}>
+          <NavButton
+            onClick={handleClick}
+            name={item.name}
+            isActive={activeItem === item.name}
+            link={item.link}
+          />
+        </div>
+      ))}
+    </aside>
+  );
 };
